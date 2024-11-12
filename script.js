2fd1efe5e2ad5e74cecec84e04eb194a04d4e3d9
Sujal-thapaa <169607669+Sujal-thapaa@users.noreply.github.com>
2024-11-11 23:58:20 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('feedback-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your feedback!');
+    // Here you can add code to send the data to a server or process it further.
+});
